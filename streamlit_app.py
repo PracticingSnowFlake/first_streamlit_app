d8ef59ff93e56c81f57f390cbbd90d3e80f380b2
Mahesh <121396856+PracticingSnowFlake@users.noreply.github.com>
2022-12-25 18:46:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title ("My parents Healthy Diner")
-streamlit.title ("My parents Healthy Diner")
-streamlit.title ("My parents Healthy Diner")
-streamlit.title ("My parents Healthy Diner")
-streamlit.title ("My parents Healthy Diner")
+streamlit.header ("My parents Healthy Diner")
+streamlit.text ("My parents Healthy Diner")
+streamlit.text ("My parents Healthy Diner")
+streamlit.text ("My parents Healthy Diner")
